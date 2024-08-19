d9b80e687f8e8ed018f7dfd6328c3d68d559d39d
SpencerBadger <spbadger@hotmail.com>
2024-08-19 18:39:35 +1000

    Cleaned up the gitignore, begun adding SwaggerUI for endpoint documentation.

--- src/backend/routes/user.js
+++ src/backend/routes/user.js
@@ -1,27 +1,76 @@
 const express = require('express')
 const router = express.Router()
 
-//GET all users
-router.get('/', (req,res) => {
-    res.json({mssg: "Retrieve all users"})
-})
 
-//GET a single user
+/**
+ * @swagger
+ * /api/user/:
+ *  get:
+ *      description: Retrieve all users.
+ *      responses:
+ *          200:
+ *              description:  Sucessfully retrieved
+ *          400:
+ *              description: Failed to retreive  
+ */
+router.get('/', (req,res) => {
+    res.send([
+        {id: 1, name: 'John Doe', email: 'john@example.com'},
+        {id: 2, name: 'Jane Doe', email: 'jane@example.com'},
+        {id: 3, name: 'Bob Smith', email: 'bob@example.com'}
+    ])
+});
+
+/**
+ *@swagger
+ * /api/user/id:
+ *  get:
+ *      description: Retrieve an individual user.
+ *      responses:
+ *        200:
+ *         description:  Sucessfully retrieved
+ *        400:
+ *          description: Failed to retrieve 
+ */
 router.get('/:id', (req,res) => {
     res.json({mssg: "Retrieve a single user"})
 })
 
-// POST a new user
+/**
+ *@swagger
+ * /api/user:
+ *  post:
+ *      description: Create a new user.
+ *      responses:
+ *          200:
+ *          description:  Sucessfully retrieved user
+ */
 router.post('/', (req,res) => {
     res.json({mssg: "Create a new user"})
 })
 
-// DELETE a user
+/**
+ *@swagger
+ * /api/user:
+ *  delete:
+ *      description: Deletes a user.
+ *      responses:
+ *          200:
+ *          description:  Sucessfully Deleted
+ */
 router.delete('/:id', (req,res) => {
     res.json({mssg: "Remove a user"})
 })
 
-// Update a user
+/**
+ *@swagger
+ * /api/user:
+ *  patch:
+ *      description: Update a user.
+ *      responses:
+ *          200:
+ *          description:  Sucessfully Updated
+ */
 router.patch('/', (req,res) => {
     res.json({mssg: "Update a user"})
 })
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,17 +1,37 @@
 require('dotenv').config()
 
-const express = require('express')
-const userRoutes = require('./routes/user')
-const tutorRoutes = require('./routes/tutor')
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const userRoutes = require('./routes/user');
+const tutorRoutes = require('./routes/tutor');
 
 
 
 // Creating express instance
-const app = express()
+const app = express();
+
+const swaggerOptions = {
+    definition: {
+        info: {
+            title: 'API for Bright Academics Tutoring Services',
+            version: '1.0.0',
+            description: 'API Documentation for Student and Tutor Management System'
+        },
+    },
+        apis: ['./routes/user.js','./routes/tutor.js']
+    }
+
+
+// Swagger setup
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 
 // middleware
 app.use(express.json())
 
+
+// For console reporting purposes temp.
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
@@ -20,8 +40,6 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/user', userRoutes)
 app.use('/api/tutor', tutorRoutes)
-
-
 
 
 // Port Listening 
