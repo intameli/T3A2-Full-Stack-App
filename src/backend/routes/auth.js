cc01b6398c263e5ccfb6f5f1fbe45cb00393fdfc
Andrios17 <alexander.andriopoulos@gmail.com>
2024-08-21 20:44:20 +1000

    fleshed out login route

--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -5,9 +5,6 @@ const bcrypt = require('bcryptjs')
 
 const router = express.Router();
 
-// router.post('/signup', authController.signup)
-// router.post('/login', authController.login)
-
 router.post('/signup', async (req, res, next) => {
     try {
         const emailHome = req.body.email;
@@ -34,8 +31,41 @@ router.post('/signup', async (req, res, next) => {
             expiresIn: '1h'
         });
 
-        res.status(201).json({ status: 'success', message: 'You have successfully signed up', token, newUser });
+        res.status(201).json({ status: 'success', message: 'You have successfully signed up', token});
 
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+});
+
+router.post('/login', async (req, res, next) => {
+    try {
+        const emailHome = req.body.email;
+        const password = req.body.password;
+
+        // Check if email or password is not provided
+        if (!emailHome ||!password) {
+            return res.status(400).json({ message: 'Email and password are required' });
+        }
+
+        // Check if user exists
+        const user = await User.findOne({ email: emailHome });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        // Check if password is correct
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        // Generate JWT
+        const token = jwt.sign({ _id: user._id },'secretkey123', {
+            expiresIn: '1h'
+        })
+
+        res.json({ status:'success', message: 'Logged in successfully', token });
     } catch (err) {
         return res.status(500).json({ error: err.message });
     }
