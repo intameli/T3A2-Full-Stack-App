95b8c063f503d0d5f97460eb965122a87cac3f72
intameli <intameli@gmail.com>
2024-08-22 13:36:57 +1000

    Revert "Spencer new backend"

---
+++ src/frontend-tutors/src/components/SignUp.jsx
@@ -0,0 +1,10 @@
+export function SignUp() {
+  return (
+    <div className="login">
+      <h3>Sign up</h3>
+      <div>Username input</div>
+      <div>Password input</div>
+      <div>Submit button</div>
+    </div>
+  );
+}
